6df7b329f80e581e4b447578dc6e072df593461d
snyh <snyh@snyh.org>
2012-02-21 20:01:08 +0800

    new file:   src/BufType.hpp 	new file:   src/pre.hpp

---
+++ src/BufType.hpp
@@ -0,0 +1,29 @@
+#ifndef __BUFTYPE_HPP_
+#define __BUFTYPE_HPP_
+#include "pre.hpp"
+
+class SendBuf {
+public:
+	void add_val(const std::string& s) {
+		tmp_.push_back(s);
+		asio_.push_back(boost::asio::buffer(tmp_[tmp_.size()-1]));
+	}
+	void add_val(void* t, size_t s) {
+		  tmp_.push_back(std::string((char*)t, s));
+		  asio_.push_back(boost::asio::buffer(tmp_[tmp_.size()-1]));
+	}
+	void add_ref(const char*data, size_t s) {
+		asio_.push_back(boost::asio::buffer(data, s));
+	}
+	std::vector<boost::asio::const_buffer>& to_asio_buf() { return asio_; }
+private:
+	std::vector<boost::asio::const_buffer> asio_;
+	std::vector<std::string> tmp_;
+};
+typedef std::shared_ptr<SendBuf> SendBufPtr;
+
+#define RECVBUFLEN (16+16+4+4+6000)  //!!需要和FInfo::chunksize保持同步
+typedef std::array<char, RECVBUFLEN> RecvBuf;
+typedef std::shared_ptr<RecvBuf> RecvBufPtr;
+
+#endif
---
+++ src/pre.hpp
@@ -0,0 +1,20 @@
+#ifndef __PRE_HPP_
+#define __PRE_HPP_
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <map>
+#include <list>
+#include <set>
+#include <array>
+#include <map>
+#include <queue>
+
+#include <boost/asio.hpp>
+#include <boost/signals2/signal.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+
+#endif
